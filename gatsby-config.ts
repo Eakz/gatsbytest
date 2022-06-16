dd958ba060a634636f23de4639bfe4b7d44c5c82
Eakz <pilpaff@gmail.com>
2022-06-16 16:50:32 +0300

    chore: finishing tutorial part2, adding typescript CSS fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,25 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [],
-}
+    siteMetadata: {
+        title: `My Gatsby Site`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
 
-export default config
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-module-resolver',
+            options: {
+                aliases: {
+                    src: './src',
+                }, // <- will be used as a root dir
+            },
+        },
+    ],
+};
+
+export default config;
